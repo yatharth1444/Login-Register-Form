2b47ae4988796f0972924eebf9bfbf1e2c268853
yatharth1444 <sherlock@sherlock.com>
2025-07-29 01:46:26 +0530

    finally completed the complete frontend

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,28 @@
+import { Route, Routes, Link } from 'react-router-dom';
+import './App.css';
+import RegisterForm from './components/Register/RegisterForm';
+import LoginForm from './components/Login/LoginForm';
 
-import './App.css'
-import RegisterForm from './components/RegisterForm'
-import LoginForm from './components/LoginForm'
 function App() {
-  
-
   return (
-    <>
-      <RegisterForm></RegisterForm>
-      <LoginForm></LoginForm>
-    </>
-  )
+    <div className="app-container">
+      <nav className="nav-links" aria-label="Authentication navigation">
+        <Link className="nav-link" to="/register">
+          Register
+        </Link>
+        <Link className="nav-link" to="/login">
+          Login
+        </Link>
+      </nav>
+
+      <main className="form-container">
+        <Routes>
+          <Route path="/register" element={<RegisterForm />} />
+          <Route path="/login" element={<LoginForm />} />
+        </Routes>
+      </main>
+    </div>
+  );
 }
 
-export default App
+export default App;
